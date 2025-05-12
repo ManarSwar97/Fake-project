28ec673e23be0e7d784f342a87646fc4b7fbceee
nmq2111 <nooraq123@gmail.com>
2025-05-12 11:37:30 +0300

    added new msg

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('Hiiiiiiiiiiiiiiiiii')
+  res.send('helooooo from the other side')
 })
 const PORT = 4000
 
